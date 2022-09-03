a6643335be654384624c6e05670ef66aacd7b5bb
tiyodakoki <kagunatsu@gmail.com>
2022-09-03 18:01:48 +0900

    テキスト型の修正

--- db/migrate/20220727032344_devise_create_users.rb
+++ db/migrate/20220727032344_devise_create_users.rb
@@ -4,10 +4,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
-      t.string :name, null: false, default: "", foreign_key: true
-      t.text :position, null: false, default: ""
+      t.string :email,              null: false
+      t.string :encrypted_password, null: false
+      t.string :name, null: false  foreign_key: true
+      t.text :position, null: false
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
